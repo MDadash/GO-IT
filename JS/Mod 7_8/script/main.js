361326bb5ba75e99d159e32078e688d7480bd00e
MargoD <margodadash@gmail.com>
2016-09-12 02:24:29 +0300

    Module 7-8 with changes

--- JS/Mod 7_8/script/main.js
+++ JS/Mod 7_8/script/main.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 
 // Tabs
 	$('.tabs__ul__li').click(function() {
-		$('.tabs__ul__li').removeClass('active');
-		$('.content p').hide();
+		$('.tabs__ul__li').removeClass('tabs__ul__li-active');
+		$('.tabs__content p').hide();
 
 		var index = $(this).data('index');
-		$('ul li:nth-child(' + index + ')').addClass('active');
-		$('.content p:nth-child(' + index + ')').show();
+		$('ul li:nth-child(' + index + ')').addClass('tabs__ul__li-active');
+		$('.tabs__content p:nth-child(' + index + ')').show();
 	});
 
 // form with tooltips	
@@ -18,11 +18,24 @@ $(document).ready(function(){
 	   		 class: 'form__element__tooltips',
 	     	 text: '' + tooltipContent + '' 
 		});
-		
-		$('.form__frame .form__element:nth-child(' + index + ')').append(mouseOverTooltips);
+		$('.form__element:nth-child(' + index + ')').append(mouseOverTooltips);
 	});
 
 	$('.form__element__input').mouseout(function(){
-		$('.form__element__tooltips').remove();
+		var index = $(this).data('index');
+		$('.form__element:nth-of-type(' + index + ')').find('.form__element__tooltips').remove();
+	});
+
+	$('.help-button').click(function(event){
+		event.preventDefault();
+		for (var i=0; i<3; i++) {
+			var index = $('.form__element__input').data('index') + i;
+			var tooltipContent = $('.form__element:nth-of-type(' + index + ') .form__element__input').data('value');
+			var mouseOverTooltips = $('<div/>', {
+		   		 class: 'form__element__tooltips',
+		     	 text: '' + tooltipContent + '' 
+			});
+			$('.form__element:nth-child(' + index + ')').append(mouseOverTooltips);
+		}
 	});
 });
